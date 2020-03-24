bf05b1fdd10a24e7decc6ff56b770ec87ccdb67a
choopi24 <36002628+choopi24@users.noreply.github.com>
2020-03-24 12:46:56 +0200

    Update beergame.js

--- beergame.js
+++ beergame.js
@@ -1,3 +1,4 @@
+//variables declaration
 let deckCount = 0;
 let dumpNumber = [];
 let dumpSuits = [];
@@ -6,8 +7,12 @@ let previousCard = '';
 let previousSuit = '';
 let Counter = 0;
 let failCheck = 0;
+let splicer = 4;
+let y = 0;
 const Cards = ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'];
-const Suits = ['Heart','Diamonds','Clovers','Spades'];
+const Suits = ['Hearts','Diamonds','Clovers','Spades'];
+
+//functions area
 window.addEventListener('load',function(){
     var ButtonsDiv = document.getElementById('buttonsArea');
     for (x = 0; x < 3; x++){
@@ -60,6 +65,8 @@ function removeButtons(x){
         document.getElementById("buttonsArea").style.color = "red";
     }
 }
+
+
 function Deck(x){
     if(x==1){
         if(Counter==0){console.log('New deck unwrapped'); Counter++;}
@@ -69,46 +76,41 @@ function Deck(x){
             console.log(randomCard + " of " + randomSuit + "(starting random)");
             
 
-
             for(j=0;j<dumpNumber.length;j++){
-                let Counter = 0;
-                while(randomCard == dumpNumber[j] && randomSuit == dumpSuits[j]){
+                console.log("checking if doubled");
+                while(randomCard === dumpNumber[j] && randomSuit === dumpSuits[j]){
+                    console.log("entered while argument");
                     randomSuit = Suits[Math.floor(Math.random()*Suits.length)];
+                    for(y=0, splicer='crap';splicer!==randomCard;y++){
+                        splicer=Cards[y];
+                        // console.log(splicer, y);
+                    }
+                    console.log(splicer, y);                    
                     console.log("suit randomized");
-                    Counter++;
-                    if(Counter>6){
-                        Cards.splice(j,1);
-                        console.log("cards spliced");
-                        randomCard = Cards[Math.floor(Math.random()*Cards.length)];                        
-                        break;
+                    j=0;
+                    // Counter++;
+                    for(x=0,Counter=0;x<=dumpNumber.length;x++){
+                        if(randomCard===dumpNumber[x]){
+                            Counter++;
+                            console.log("found in array " + Counter + " times");
+                        }
+                        if(Counter==4){
+                            Cards.splice(y-1,1);
+                            randomCard = Cards[Math.floor(Math.random()*Cards.length)];
+                            j=0;
+                            console.log("cards spliced");
+                            console.log(Cards);
+                            x=Cards.length+3;
+                            Counter = 0;
+                        }
                     }
                 }
-                console.log(randomCard + " of " + randomSuit + "(ending random)");                
             }
-            dumpNumber += randomCard;
-            dumpSuits += randomSuit;
+            console.log(randomCard + " of " + randomSuit + "(ending random)");            
+            dumpNumber.push(randomCard);
+            console.log(dumpNumber);
+            dumpSuits.push(randomSuit);
+            if(dumpNumber.length===52){console.log("deck out of cards")}
         }
     }
 }
-// function CheckSuits(chosenCard,chosenSuit){
-//     for(j=0;j<dumpNumber.length;j++){
-//         let Counter = 0;
-//         while(chosenCard==dumpNumber[j] && chosenSuit == dumpSuits[j]){
-//             console.log("Duplicate Found and randomized");
-//             randomSuit = Suits[Math.floor(Math.random()*Suits.length)];
-//             console.log("new suit = " + randomSuit);
-//             Counter++;
-//             if(counter>7){
-//                 console.log("removing number from array");
-//                 console.log("array before removal: " + dumpNumber);
-//                 dumpNumber.splice()
-//                 console.log("array after removal: " + dumpNumber);
-                
-//             }
-//         }
-//     }
-// }
-// function removeNum(){
-
-// }
-//ברגע שמתגלה שהמספר יצא כבר בארבעת סוגיו, יש להוריד אותו מהמערך.
